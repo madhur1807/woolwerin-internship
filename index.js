34f6db7e74e4a16e873d22f7cd1a9bcf586b8131
madhur1807 <madhurmittal1807@gmail.com>
2018-06-24 22:51:53 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(()=>{    
-    
+    $(".container").css("opacity", "0.2");
     getMembers();
 });
 
@@ -28,30 +28,23 @@ function setCardsTeamA(a_members){
         var items = '<div class="item"><div class="card" style="width: 18rem;"><img class="card-img-top" src='+a_members[i]["val"]["member_image"]+' alt="Card image cap"><div class="card-body"><h5 class="card-title">'+a_members[i]["val"]["member_name"]+'</h5><h6 class="card-subtitle mb-2 text-muted">'+a_members[i]["val"]["member_designation"]+','+a_members[i]["val"]["member_team"]+'</h6><p class="card-text">'+a_members[i]["val"]["member_brief"]+'</p></div></div></div>';
         $("#teamA").append(items);
     }
-    $(".owl-carousel").owlCarousel({
-        loop:true,
-        responsiveClass:true,
-        autoPlay : 3000,
-        dots : true,
-        nav : true
-    });
-    $(".owl-prev").css("width", "2%");
-    $(".owl-next").css("width", "2%");
+   
 }
 
 function setCardsTeamB(b_members){    
     var len = b_members.length;
     console.log(len);
     for(let i=0; i<len; i++){
-        var items = '<div class="item"><div class="card" style="width: 18rem;"><img class="card-img-top" src='+b_members[i]["val"]["member_image"]+' alt="Card image cap"><div class="card-body"><h5 class="card-title">'+b_members[i]["val"]["member_name"]+'</h5><h6 class="card-subtitle mb-2 text-muted">'+b_members[i]["val"]["member_designation"]+','+b_members[i]["val"]["member_team"]+'</h6><p class="card-text">'+b_members[i]["val"]["member_brief"]+'</p></div></div></div>';
+        var items = '<div class="card" style="width: 18rem;"><img class="card-img-top" src='+b_members[i]["val"]["member_image"]+' alt="Card image cap"><div class="card-body"><h5 class="card-title">'+b_members[i]["val"]["member_name"]+'</h5><h6 class="card-subtitle mb-2 text-muted">'+b_members[i]["val"]["member_designation"]+','+b_members[i]["val"]["member_team"]+'</h6><p class="card-text">'+b_members[i]["val"]["member_brief"]+'</p></div></div>';
         $("#teamB").append(items);
     }
     $(".owl-carousel").owlCarousel({
         loop:true,
         autoPlay : 3000,
-        dots : true,
         nav : true
     });
     $(".owl-prev").css("width", "2%");
     $(".owl-next").css("width", "2%");
+    $(".loader").hide();
+    $(".container").css("opacity", "1");
 }
